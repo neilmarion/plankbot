56ecd2cd1900ee6c067c3140c014c3707e4822ec
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2020-01-29 18:15:48 +0800

    Upgrade to switch to Pivotal Tracker

--- app/services/plankbot/load_releases.rb
+++ app/services/plankbot/load_releases.rb
@@ -1,23 +1,23 @@
 module Plankbot
   class LoadReleases
     def self.execute(transformed_releases)
-
       transformed_releases.each do |transformed_release|
         release = Release.create({
           name: transformed_release["name"],
           team: transformed_release["team"],
           jira_id: transformed_release["id"],
-          description: transformed_release["description"] || "",
-          start_date: transformed_release["startDate"],
-          release_date: transformed_release["releaseDate"],
+          description: transformed_release["name"] || "",
+          start_date: transformed_release["created_at"],
+          release_date: transformed_release["accepted_at"],
         })
 
         transformed_release["issues"].each do |issue|
+          next unless issue[:current_state] == "accepted"
           release.release_issues.create(
             summary: issue[:summary] || "",
-            key: issue[:key] || "",
             jira_id: issue[:jira_id] || "",
             issue_type: issue[:issue_type] || "",
+            key: issue[:key] || "",
           )
         end
 
